b6ef8584978e9b0085ed9690031e6037752d500a
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-03-10 13:52:15 +0100

    feat() adding temporary functionality (main)

--- src/main/java/com/trl/vertx_postgresql/config/PostgreSqlConfig.java
+++ src/main/java/com/trl/vertx_postgresql/config/PostgreSqlConfig.java
@@ -7,12 +7,24 @@ public final class PostgreSqlConfig {
   private PostgreSqlConfig() {
   }
 
+//  private static final String DB_HOST = System.getenv().getOrDefault("db-host", "localhost");
+//  private static final int DB_PORT = Integer.parseInt(System.getenv().getOrDefault("db-port", "5432"));
+//  private static final String  DB_NAME = System.getenv().getOrDefault("db-name", "vertx-postgresql-maven-db");
+//  private static final String  DB_USER_NAME = System.getenv().getOrDefault("db-user-name", "developer");
+//  private static final String  DB_PASSWORD = System.getenv().getOrDefault("db-password", "123");
+
+  private static final String DB_HOST = System.getProperty("db-host", "localhost");
+  private static final int DB_PORT = Integer.parseInt(System.getProperty("db-port", "5432"));
+  private static final String  DB_NAME = System.getProperty("db-name", "vertx-postgresql-maven-db");
+  private static final String  DB_USER_NAME = System.getProperty("db-user-name", "developer");
+  private static final String  DB_PASSWORD = System.getProperty("db-password", "123");
+
   public static PgConnectOptions pgConnectOpts() {
     return new PgConnectOptions()
-      .setHost("localhost")
-      .setPort(5432)
-      .setDatabase("vertx-postgresql-maven-db")
-      .setUser("developer")
-      .setPassword("123");
+      .setHost(DB_HOST)
+      .setPort(DB_PORT)
+      .setDatabase(DB_NAME)
+      .setUser(DB_USER_NAME)
+      .setPassword(DB_PASSWORD);
   }
 }
